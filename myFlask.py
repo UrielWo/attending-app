e468bd3c5c9f6fce0952a5c170b42aeb7e7ec1b6
UrielWo <urielwo@gmail.com>
2022-09-07 11:21:18 +0300

    add date submit input

--- myFlask.py
+++ myFlask.py
@@ -1,13 +1,15 @@
-from flask import Flask
-
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route('/Attendance', methods = ['POST', 'GET'])
 
-def hello_world():
-    return "Hello!"
+def Attendance():
+	if request.method == 'POST':
+		date = request.form['ed']
+		return date
 
-app.add_url_rule('/', 'hello',hello_world)
+app.add_url_rule('/', 'Attendance',Attendance) #add the url to the localhost (http://127.0.0.1:5000/hello)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
+	

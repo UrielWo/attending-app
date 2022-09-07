5b30a1761395af73a8387af57067bf9fef3c15b8
UrielWo <urielwo@gmail.com>
2022-09-07 11:35:41 +0300

    update flask

--- myFlask.py
+++ myFlask.py
@@ -8,8 +8,11 @@ def Attendance():
 		date = request.form['ed']
 		return date
 
-app.add_url_rule('/', 'Attendance',Attendance) #add the url to the localhost (http://127.0.0.1:5000/hello)
+app.add_url_rule('/', 'Attendance',Attendance) #add the url to the localhost (http://127.0.0.1:5000/Attendance)
 
 if __name__ == '__main__':
 	app.run(debug=True)
-	
+
+
+# the html and the py speaking!!
+# dont forget do test!

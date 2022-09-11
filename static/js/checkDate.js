93cafde6b1807215ebead02705ad610f7adb033d
UrielWo <urielwo@gmail.com>
2022-09-12 01:16:19 +0300

    update

---
+++ static/js/checkDate.js
@@ -0,0 +1,9 @@
+function checkSubmit() {
+      alert("create an if function about the date ")
+  /*var x = document.getElementById("sub");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }*/
+}

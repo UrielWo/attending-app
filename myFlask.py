9c1d3a7f1511f65b3db500e69c50d508fcc3beec
UrielWo <urielwo@gmail.com>
2022-09-13 16:29:36 +0300

    multi attendance from database working

--- myFlask.py
+++ myFlask.py
@@ -24,6 +24,8 @@ mycursor.execute("USE course_Attendance;")
 
 @app.route('/Main')
 def main():
+    mycursor.execute("SELECT DISTINCT mid(SUBSTRING_INDEX(Meeting_Start_Time,' ',1),3) FROM attendance_data2 WHERE mid(SUBSTRING_INDEX(Meeting_Start_Time,' ',1),3);")
+    dates = mycursor.fetchall() #check how to send the list to drop list in html
    return render_template("Main.html")
 @app.route('/Database_table', methods=("POST", "GET"))
 def data_table():
@@ -43,9 +45,39 @@ def data_table():
         pivot['Time_On_Lesson'] = pivot['Time_On_Lesson'].astype(str) + '%'  # add % to percent
         return render_template('simple.html',  tables=[pivot.to_html(classes='data')], titles="")
         client.close()
-
 @app.route('/Integrated_table')
 def integrated_table():
+    integrated_Table = pd.DataFrame()
+    mycursor.execute("SELECT DISTINCT mid(SUBSTRING_INDEX(Meeting_Start_Time,' ',1),3) FROM attendance_data2 WHERE mid(SUBSTRING_INDEX(Meeting_Start_Time,' ',1),3);")
+    dates = mycursor.fetchall()
+    #return dates# list of lists
+    #dates = [["2022-08-01"],["2022-08-02"],["2022-07-26"]]
+    for d in dates:
+        mycursor.execute("SELECT * FROM attendance_data2 WHERE mid(SUBSTRING_INDEX(Meeting_Start_Time,' ',1),3) = %s ;", d)
+        records = mycursor.fetchall()
+        df = pd.DataFrame(
+            columns=['Meeting_Name', 'Meeting_Start_Time', 'Meeting_End_Time', 'Name', 'Attendee_Email', 'Join_Time',
+                     'Leave_Time', 'Attendance_Duration', 'Connection_Type'])
+        for row in records:
+            df.loc[len(df)] = row
+        attendance.duration_Col(df)  # split Attendance Duration to use min as int for calculations
+        attendance.private_cases(df)  # Convert Hebrew names to English
+        Atten_Duration = (df['Duration'].max())  # get max value of from duration column
+        pivot = attendance.single_Pivot_Parse(df, Atten_Duration)
+        integrated_Table = pd.concat([integrated_Table, pivot], axis=0)
+    integrated_Count = attendance.Daily_Attendence(integrated_Table)
+    integrated_Pivot = attendance.integrated_Table_Parse(integrated_Table)
+    final_Table = attendance.merge_daily_to_time(integrated_Count, integrated_Pivot)
+    return render_template('summary.html', tables=[final_Table.to_html(classes='data')], titles="")
+
+    #return integrated_Table
+
+
+
+
+
+"""@app.route('/Integrated_table')
+def integrated_table():                      #version 2
     Atten_Duration = {}
     mycursor.execute("SELECT * FROM attendance_data2")
     records = mycursor.fetchall()
@@ -75,7 +107,7 @@ def integrated_table():
     return (Atten_Duration)
 
 
-    """integrated_Count = attendance.Daily_Attendence(df)
+    integrated_Count = attendance.Daily_Attendence(df)
     attendance.duration_Col(df)
     #Atten_Duration = (df['Duration'].max())# not true
     #f = df.assign(Time_On_Lesson=lambda x: round(x['Duration'] / Atten_Duration * 100))  # calculate percent of attendence

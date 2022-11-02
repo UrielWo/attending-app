45820f2006fed81da8a9c18dca6e17f6a63c12db
UrielWo <urielwo@gmail.com>
2022-11-02 16:58:50 +0200

    deploy.sh update

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ if [[ -n "$1" ]] ; then
         echo testing;
         # copy files to test server / need to add tests file
         sudo scp docker-compose.yml .env ec2-user@test:/home/ec2-user/Project_test
-        sudo ssh -t ec2-user@test " ip -4 a && exit "
+        sudo ssh -tt ec2-user@test " ip -4 a && exit "
         echo finish tests successfully
         ip -4 a
         ' :

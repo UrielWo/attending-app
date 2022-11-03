8491c7c48eca5efc385e288a162a4557ede598d1
UrielWo <urielwo@gmail.com>
2022-11-03 12:35:35 +0200

    test page

--- tests.sh
+++ tests.sh
@@ -5,7 +5,15 @@ if [ -d "$DIR" ]; then
     docker pull urielwo/final_project:latest
     docker images
     docker-compose up -d
-    # run tests
+    # load .env password // if IP change need to updated on the jenkins manchine
+    export $(cat .env | xargs)
+    # test 1
+    if curl -I "http://"${TESTVM_PUB_IP}":8081/Main" 2>&1 | grep -w "200\|301" ; then
+        echo "page is up"
+    else
+        echo "page is down"
+    fi
+    # test 2
 else
     echo Directory "$DIR" not exists.
     exit 2 

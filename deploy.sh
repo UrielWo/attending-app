251ca0436121246e95643f7621b6bc8e7a502906
UrielWo <96925853+UrielWo@users.noreply.github.com>
2022-11-09 19:15:04 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,16 +8,14 @@ DIR2="/home/ec2-user/Project_prod/"
 if [[ -n "$1" ]] ; then
    if [ "$arg1" = "test" ]; then
         echo testing;
-        docker system prune -a --volumes -f
         # copy files to test server / need to add tests file
         sudo scp docker-compose.yml .env tests.sh ec2-user@test:/home/ec2-user/Project_test
-        sudo ssh -tt ec2-user@test " cd "$DIR" && bash tests.sh && exit "
+        sudo ssh -tt ec2-user@test " cd "$DIR" && docker system prune -a --volumes -f && bash tests.sh && exit "
         echo finish tests successfully
    elif [ "$arg1" = "prod" ]; then
        echo product;
-       docker system prune -a --volumes -f
        sudo scp docker-compose.yml .env ec2-user@prod:/home/ec2-user/Project_prod
-       sudo ssh -tt ec2-user@prod " cd "$DIR2" && docker pull urielwo/final_project:latest && docker-compose up -d && exit "
+       sudo ssh -tt ec2-user@prod " cd "$DIR2" && docker system prune -a --volumes -f && docker pull urielwo/final_project:latest && docker-compose up -d && exit "
    else
        echo invalid argument
    fi 
